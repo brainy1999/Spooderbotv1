240ce5272e515e888c8be8532cd4a800e29356d5
Bernard Li <brainy1999@users.noreply.github.com>
2016-10-13 18:13:10 -0500

    Add files via upload

---
+++ spiderbotcodev2.ino
@@ -0,0 +1,30 @@
+//LI INDUSTRIES
+//LIS - SOFTWARE DIVISION
+#include <XBOXRECV.h>
+#include <XBOXUSB.h>
+#include <MotorDriver.h>
+#include <seeed_pwm.h>
+
+
+MotorDriver motor;
+USB Usb;
+
+void setup()
+{
+
+  Serial.begin(9600);
+  motor.begin();
+  motor.speed(1,100);
+  delay(1000);
+  motor.stop(1);
+}
+ 
+void loop()
+{
+    if(XboxRCV.Xbox360Connected[0]) {
+      if(Xbox.getButtonClick(i,L2)){motor.speed(1, -100);}
+
+     else{motor.stop(1);}
+
+}
+
